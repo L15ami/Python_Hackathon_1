6ab77d9cafb33692018762504c0058a6a5a34f5b
Himanish15 <amiteshloll@gmail.com>
2023-12-07 19:16:19 +0400

    check weather condition

--- weather/script.js
+++ weather/script.js
@@ -1,75 +1,3 @@
-// const app = document.querySelector('.weather-app');
-// const temp = document.querySelector('.temp');
-// const dateOutput = document.querySelector('.date');
-// const timeOutput = document.querySelector('.time');
-// const conditionOutput = document.querySelector('.condition');
-// const nameOutput = document.querySelector('.name');
-// const icon = document.querySelector('.icon');
-// const cloudOutput = document.querySelector('.cloud');
-// const humidityOutput = document.querySelector('.humidity');
-// const windOutput = document.querySelector('.wind');
-// const form = document.querySelector('#location-input');
-// const search = document.querySelector('.search');
-// const btn = document.querySelector('.submit');
-// const cities = document.querySelectorAll('.city');
-// const apiKey = "1cf797c92f47eac76103910f1342e808"
-// // defaut city when page loads
-// let cityInput = "London";
-
-// // add click event to each city in the pannel
-// cities.forEach((city) => {
-//     city.addEventListener('click', (e) => {
-//         // change from default city to the click one
-//         cityInput = e.target.textContent;
-//         // call the function to get the weather data
-//         fetchWeatherData();
-//         app.style.opacity = "0";
-//     });
-// })
-
-// // add submit event to the form
-// form.addEventListener('submit', (e) => {
-//     // if search bar is empty, throw an alert
-//     if (search.Value.length == 0 ){
-//         alert("Please enter a city name");
-//     }else {
-//         // change from default city to the input one
-//         cityInput = search.value;
-//         // call the function to get the weather data
-//         fetchWeatherData();
-//         search.value = "";
-//         app.style.opacity = "0";
-//     }
-//     e.preventDefault();
-// });
-
-// // function that return a day of the week
-
-// function dayOfTheWeek(day, month, year){
-//     const weekday = [
-//         "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
-//     ];
-//     return weekday [new Date(`${day}/${month}/${year}`).getDay()];
-// };
-
-// // function that fetches and display the data from the weather API
-
-// function fetchWeatherData() {
-//     fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&limit=1&appid=${apiKey}`)
-//         .then(response => response.json())
-//         .then(data => {
-//             console.log(data);
-//         // add temperature and weather condition to the page
-//         temp.innerHTML = `${data.main.temp} &#176;c`;
-//         conditionOutput.innerHTML = data.weather[0].description;
-//         })
-// }
-// fetchWeatherData()
-
-
-
-
-
 const app = document.querySelector('.weather-app');
 const temp = document.querySelector('.temp');
 const dateOutput = document.querySelector('.date');
@@ -162,9 +90,9 @@ function fetchWeatherData() {
          backgroundImage = 'url(./images/day/sunny.jpg)';
      } else if (weatherCondition === 'clouds' && isDay) {
          backgroundImage = 'url(./images/day/cloudy.jpg)';
-     } else if (weatherCondition === 'rain') {
+     } else if (weatherCondition === 'rain' && weatherCondition.includes('light rain')) {
          backgroundImage = 'url(./images/rainy.jpg)';
-     } else if (weatherCondition === 'snow') {
+     } else if (weatherCondition === 'snow'&& weatherCondition.includes('light snow')) {
          backgroundImage = 'url(./images/snow.jpg)';
      } else if (weatherCondition === 'clear' && !isDay) {
          backgroundImage = 'url(./images/night/clear.jpg)';
@@ -178,6 +106,7 @@ function fetchWeatherData() {
          // Default background image if no specific condition is matched
          backgroundImage = 'url(./images/night/bg.jpg)';
      }
+
  
      // Set the background image
      document.body.style.backgroundImage = backgroundImage;
